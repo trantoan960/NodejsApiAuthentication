7207693897b6384b16c17f8a22cd345e3d8fcc4e
Sky Albert <trantoan.fox.97@gmail.com>
2020-04-29 18:14:24 +0800

    Commit finish video 09.

--- configs/index.js
+++ configs/index.js
@@ -2,8 +2,8 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   auth: {
     google: {
-      CLIENT_ID: '160225779697-bg3pvqpc95bvjnu7p7gvquati27ga68m.apps.googleusercontent.com',
-      CLIENT_SECRET: 'lBcL3OsWTUqTVeTAHkOPzDzU'
+      CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
     }
   }
 }
